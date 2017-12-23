b8c359d00be061d5537714cae03f20f25cf60e27
A B Owen <owen@stanford.edu>
2017-12-23 14:02:07 -0800

    fixed bug and added a pdf

--- latt.R
+++ latt.R
@@ -2,9 +2,9 @@ latt = function(n0=244,n1=552){
 # Make a two d lattice
 
 x1 = 0:(n1-1)
-x2 = x1 * n0 %% n1
+x2 = (x1 * n0) %% n1
 pdf("foo.pdf")
-points( x1/n1, x2/n2,pch=20)
+plot( x1/n1, x2/n1,pch=20)
 dev.off()
 
 }
